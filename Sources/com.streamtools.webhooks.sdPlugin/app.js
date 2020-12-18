8ab809eb4701adce7339a74a235b1a3da3a9105c
Alberto González <alberto@gcatalan.com>
2020-12-18 16:07:23 +0100

    Final version

--- Sources/com.streamtools.webhooks.sdPlugin/app.js
+++ Sources/com.streamtools.webhooks.sdPlugin/app.js
@@ -22,6 +22,21 @@ const action = {
         console.log(`[onSendToPlugin] ${JSON.stringify(jsonObj)}`);
         if(jsonObj.payload) {
             $SD.api.setSettings(jsonObj.context, jsonObj.payload);
+            
+            let icon = "action@2x";
+            let url = "https://app.streamtools.com/Resources/App/Theme/STools/img/{icon}.png";
+            if (jsonObj.payload['widgettype'] == 'flipcoin') {
+                icon = "C11B_Coin_head";
+                url = "https://app.streamtools.com/Resources/App/Theme/STools/img/flipcoin/{icon}_a.png";
+            } else if (jsonObj.payload['widgettype'] == 'diceroller') {
+                icon = "C12B_Dice_1";
+                url = "https://app.streamtools.com/Resources/App/Theme/STools/img/diceroller/{icon}_a.png";
+            }
+            toDataURL(url.replace("{icon}", icon),
+                function (dataUrl) {
+                    $SD.api.setImage(jsonObj.context, dataUrl);
+                }
+            );
         }
     },
     onKeyUp: (jsonObj) => {
@@ -30,7 +45,7 @@ const action = {
             $SD.api.showAlert(jsonObj.context);
             return;
         }
-        let url = 'http://streamtools.local.com/webhook/'+jsonObj.payload.settings.widgettype+'/'+jsonObj.payload.settings.widgetid+'/'; 
+        let url = 'https://app.streamtools.com/webhook/'+jsonObj.payload.settings.widgettype+'/'+jsonObj.payload.settings.widgetid+'/'; 
         fetch(url, {
             "method": 'POST',
             "headers": {
@@ -41,3 +56,23 @@ const action = {
         }).then(result => $SD.api.showOk(jsonObj.context), error => $SD.api.showAlert(jsonObj.context));
     }
 };
+
+function toDataURL(src, callback, outputFormat) {
+    let img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function () {
+        let canvas = document.createElement('CANVAS');
+        let ctx = canvas.getContext('2d');
+        let dataURL;
+        canvas.height = this.naturalHeight;
+        canvas.width = this.naturalWidth;
+        ctx.drawImage(this, 0, 0);
+        dataURL = canvas.toDataURL(outputFormat);
+        callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+        img.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        img.src = src;
+    }
+}
